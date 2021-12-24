e716e3f4d195a26f20aba58721fe76831c775203
Dipta ranjan panda <64889320+wlommusic@users.noreply.github.com>
2021-12-24 15:00:08 +0530

    new branch

--- 1 searching algorithms/searching algo - linearsearch.js
+++
@@ -1,9 +0,0 @@
-function linear(arr,val){
-    for (i =0;i<arr.length;i++){
-        if(arr[i]==val){
-            return i
-        }
-    }return -1
-}
-
-linear([0,1,3,4,5],4)
--- 1 searching algorithms/searching algo - linearsearch.py
+++
@@ -1,6 +0,0 @@
-def linear (arr,val):
-  for i in range(len(arr)):
-    if arr[i] == val:
-      return i
-  return -1  
-print(linear([0,1,2,3,4],3) )     
